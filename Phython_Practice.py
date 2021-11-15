1e31bdcdd84947c69373db75346c5047ba44f158
Marcio <92833805+mciano@users.noreply.github.com>
2021-11-14 20:38:21 -0500

    added csv file

--- Phython_Practice.py
+++ Phython_Practice.py
@@ -1,1 +1,25 @@
 print("Hello World")
+
+
+counties = ["Arapahoe","Denver","Jefferson"]
+if counties[1] == 'Denver':
+    print(counties[1])
+
+
+counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county in counties_dict:
+    print(county)
+
+
+for county in counties_dict.keys():
+    print(county)
+
+
+for county in counties_dict:
+    print(counties_dict[county])
